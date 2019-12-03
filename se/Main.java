31b5da3f82e022dda5cea2b6d8fd8005644d00fc
Seyoung Koo <43868512+koosy123@users.noreply.github.com>
2019-12-03 16:06:55 +0900

    Update Main.java

--- se/Main.java
+++ se/Main.java
@@ -2,6 +2,8 @@ package se;
 
 // 1: remote by koosy123
 // 1: local by koosy123
+
+// 1: localBranch01 by koosy123
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
